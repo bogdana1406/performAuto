7d94afebc53989528c02e461f826f0ed43f84344
Mikhail <mihozfe@gmail.com>
2018-10-05 09:35:14 +0300

    fixed counter markup

--- resources/views/includes/counter.blade.php
+++ resources/views/includes/counter.blade.php
@@ -1,26 +1,26 @@
 <div class="counter d-flex rounded">
 	<div class="col d-flex">
-		<div class="count-box">
+		<div class="count-box mr-4">
 			<div class="count">500</div>
 		</div>
-		<span>@lang('counter.count-box1')</span>
+		<span class="align-self-end text-left">@lang('counter.count-box1')</span>
 	</div>
 	<div class="col d-flex">
-		<div class="count-box">
+		<div class="count-box mr-4">
 			<div class="count">70</div>
 		</div>
-		<span>@lang('counter.count-box2')</span>
+		<span class="align-self-end text-left">@lang('counter.count-box2')</span>
 	</div>
 	<div class="col d-flex">
-		<div class="count-box">
+		<div class="count-box mr-4">
 			<div class="count">12</div>
 		</div>
-		<span>@lang('counter.count-box3')</span>
+		<span class="align-self-end text-left">@lang('counter.count-box3')</span>
 	</div>
 	<div class="col d-flex">
-		<div class="count-box">
+		<div class="count-box mr-4">
 			<div class="count">50</div>
 		</div>
-		<span>@lang('counter.count-box4')</span>
+		<span class="align-self-end text-left">@lang('counter.count-box4')</span>
 	</div>
 </div>
